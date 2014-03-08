d1ba30d45a879e0cd1f3fa5c388aedc5e93a1090
malin <malin@piatka.pl>
2014-03-08 22:34:17 +0100

    SetUp(Fixture)

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -10,6 +14,9 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
@@ -21,5 +28,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.wasRun)
 
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
