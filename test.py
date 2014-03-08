b8c3484e02ceaff2f02121482ea01a63f3370d67
malin <malin@piatka.pl>
2014-03-08 23:29:21 +0100

    Supporting test for TestResult

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class TestCase:
     def __init__(self, name):
@@ -57,6 +61,12 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 0 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         result = test.run()
@@ -64,5 +74,6 @@ class TestCaseTest(TestCase):
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+TestCaseTest("testFailedResultFormatting").run()
 #TestCaseTest("testFailedResult").run()
 
